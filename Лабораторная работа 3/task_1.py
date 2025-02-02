ae88f9403affb4fc15987bcd333a965eac21ebfa
Drucuma <sofija230904@gmail.com>
2025-02-02 18:17:28 +0300

    Лабораторная работа 3

--- Лабораторная работа 3/task_1.py
+++ Лабораторная работа 3/task_1.py
@@ -1,14 +1,83 @@
-# TODO Напишите функцию для поиска индекса товара
-def search(lists, index):
-    for i in range(len(lists)):
-        if lists[i] == index:
-            return i
+class Book:
+    """ Базовый класс книги. """
 
-items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+    def __init__(self, name: str, author: str):
+        # Инициализация name и author
+        self._name = name
+        self._author = author
 
-for find_item in ['банан', 'груша', 'персик']:
-    index_item = search(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
-    if index_item is not None:
-        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
-    else:
-        print(f"Товар '{find_item}' не найден в списке.")
+    @property
+    def name(self):
+        # Свойство для чтения name
+        return self._name
+
+    @property
+    def author(self):
+        # Свойство для чтения author
+        return self._author
+
+    def __str__(self):
+        # Строковое представление
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        # Оф строковое представление
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        # Вызываем конструктор базового класса
+        super().__init__(name, author)
+        # Инициализация pages
+        self.pages = pages
+
+    @property
+    def pages(self):
+        # Свойство для чтения pages
+        return self._pages
+
+    @pages.setter
+    def pages(self, value):
+        # Проверка на тип и значение для pages
+        if not isinstance(value, int):
+            raise TypeError("Количество страниц должно быть целым числом")
+        if value <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = value
+
+    def __str__(self):
+        # Строковое представление
+        return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        # Оф строковое представление
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        # Вызываем конструктор базового класса
+        super().__init__(name, author)
+        # Инициализация duration
+        self.duration = duration
+
+    @property
+    def duration(self):
+        # Свойство для чтения duration
+        return self._duration
+
+    @duration.setter
+    def duration(self, value):
+        # Проверка на тип и значение для duration
+        if not isinstance(value, (int, float)):
+            raise TypeError("Продолжительность должна быть числом с плавающей запятой")
+        if value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = value
+
+    def __str__(self):
+        # Строковое представление
+        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration} часов"
+
+    def __repr__(self):
+        # Оф строковое представление
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
